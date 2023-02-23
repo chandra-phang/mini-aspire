f7d8d19fb0ecb307d43508ed1edf39bedbbab6b7
Chandra Phang <chandra.2@circles.asia>
2023-02-23 12:36:09 +0700

    Add user seed data for customer and admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,46 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->delete();
+ 
+        // Create Customer 1
+        User::create(array(
+            'name' => 'Customer 1',
+            'email' => 'customer1@gmail.com',
+            'is_admin' => false,
+            'password' => Hash::make('0123456789'),
+        ));
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create Customer 2
+        User::create(array(
+            'name' => 'Customer 2',
+            'email' => 'customer2@gmail.com',
+            'is_admin' => false,
+            'password' => Hash::make('0123456789'),
+        ));
+
+        // Create Admin 1
+        User::create(array(
+            'name' => 'Admin 1',
+            'email' => 'admin1@gmail.com',
+            'is_admin' => true,
+            'password' => Hash::make('0123456789'),
+        ));
+
+        // Create Admin 2
+        User::create(array(
+            'name' => 'Admin 2',
+            'email' => 'admin2@gmail.com',
+            'is_admin' => true,
+            'password' => Hash::make('0123456789'),
+        ));
     }
 }
