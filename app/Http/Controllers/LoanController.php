e9b97f36cb5a2fc80435eb93bb4dc5467b097df5
Chandra Phang <chandra.2@circles.asia>
2023-02-23 17:57:01 +0700

    Update customer cash_balance after loan is approved

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
 use Carbon\Carbon;
 
 use App\Models\Loan;
 use App\Models\ScheduledRepayment;
+use App\Models\User;
 
 class LoanController extends Controller
 {
@@ -31,7 +33,7 @@ class LoanController extends Controller
     // Display a listing of the loans by customer_id.
     public function customer_index()
     {
-        $loans = Loan::Where('customer_id', auth()->user()->id);
+        $loans = Loan::Where('customer_id', auth()->user()->id)->get();
         return response()->json([
             "success" => true,
             "data" => $loans,
@@ -42,32 +44,34 @@ class LoanController extends Controller
     public function store(Request $request)
     {
         try {
-            //Validated
-            $validateLoan = Validator::make($request->all(), 
+            // Validated request body
+            $validate = Validator::make($request->all(), 
             [
-                'amount_required' => 'required',
+                'total_amount' => 'required',
                 'loan_term' => 'required',
             ]);
 
-            if($validateLoan->fails()){
+            if($validate->fails()){
                 return response()->json([
                     'success' => false,
                     'message' => 'validation error',
-                    'errors' => $validateLoan->errors()
+                    'errors' => $validate->errors()
                 ], 422);
             }
 
-            $loan = Loan::create([
-                'amount_required' => $request->amount_required,
+            $userID = auth()->user()->id;
+
+            $loan = Loan::Create([
+                'total_amount' => $request->total_amount,
                 'loan_term' => $request->loan_term,
                 'status' => 'PENDING',
-                'customer_id' => auth()->user()->id,
+                'customer_id' => $userID,
             ]);
 
             $data = Loan::FindOrFail($loan->id);
             if ($data) {
-                $scheduledPayableAmount = $loan->amount_required/$loan->loan_term;
-                $scheduledPayableAmount = (int)$scheduledPayableAmount;
+                $scheduledPayableAmount = $loan->total_amount/$loan->loan_term;
+                $scheduledPayableAmount = round($scheduledPayableAmount, 2);
 
                 for($i = 0; $i<$loan->loan_term; $i++)
                 {
@@ -76,13 +80,14 @@ class LoanController extends Controller
                     $dueDate = $todayDate->addWeeks($i+1);
 
                     if (($i + 1) == $loan->loan_term) {
-                        $scheduledPayableAmount = $loan->amount_required - ($i * $scheduledPayableAmount);
+                        $scheduledPayableAmount = $loan->total_amount - ($i * $scheduledPayableAmount);
                     };
 
                     ScheduledRepayment::Create([
                         'loan_id' => $loan->id,
-                        'amount_required' => $scheduledPayableAmount,
-                        'amount_paid' => 0,
+                        'customer_id' => $userID,
+                        'payable_amount' => $scheduledPayableAmount,
+                        'paid_amount' => 0,
                         'due_date' => $dueDate,
                         'status' => 'PENDING',
                     ]);
@@ -90,7 +95,7 @@ class LoanController extends Controller
 
                 return response()->json([
                     'status' => true,
-                    'message' => 'Loan created successfully!',
+                    'data' => $loan,
                 ], 200);
             } else {
                 return response()->json([
@@ -114,7 +119,10 @@ class LoanController extends Controller
         if (auth()->user()->is_admin) {
             $loan = Loan::Find($id);
         } else {
-            $loans = Loan::Where(['id' => $id, 'customer_id' => auth()->user()->id])->get();
+            $loans = Loan::Where([
+                'id' => $id,
+                'customer_id' => auth()->user()->id,
+            ])->get();
             if (count($loans) > 0) {
                 $loan = $loans[0];
             }
@@ -144,14 +152,16 @@ class LoanController extends Controller
         }
 
         try {
-            $loan = Loan::FindOrFail($id);
+            $loans = Loan::Where('id', $id)->get();
             // Validate whether loan is exist 
-            if (!$loan) {
+            if (count($loans) == 0) {
                 return response()->json([
                     "success" => false,
                     "message" => "Loan not found",
                 ], 404);
             }
+
+            $loan = $loans[0];
 
             // Validate customer and approver can't be same persone
             if ($loan->customer_id == auth()->user()->id){
@@ -163,15 +173,20 @@ class LoanController extends Controller
 
             // Validate loan status is PENDING
             if ($loan->status == 'PENDING') {
-                $loan->update([
+                $loan->Update([
                     'approver_id' => auth()->user()->id,
                     'approved_at' => Carbon::now(),
                     'status' => 'APPROVED',
                 ]);
 
+                // Update customer cash balance
+                $customer = User::Find($loan->customer_id);
+                $cashBalance = $customer->cash_balance - $loan->total_amount;
+                $customer->Update(['cash_balance' => $cashBalance]);
+
                 return response()->json([
                     'status' => true,
-                    'message' => 'Loan approved successfully!',
+                    'message' => $loan,
                 ], 200);
             } else if ($loan->status == 'APPROVED') {
                 return response()->json([
